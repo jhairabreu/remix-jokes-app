20ffc9a1b23a3a853aa7d09355c73560c89eed4a
Jhair Abreu <jhair.abreu@farfetch.com>
2022-02-15 16:56:35 +0000

    add route loaders

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,32 @@
+import type { LinksFunction, LoaderFunction } from "remix";
+import { useLoaderData } from "remix";
+import { db } from "~/utils/db.server";
+import stylesUrl from "~/styles/jokes.css";
+import type { Joke } from "@prisma/client";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: stylesUrl }];
+};
+
+type LoaderData = { joke: Joke };
+
+export const loader: LoaderFunction = async ({ params }) => {
+  const joke = await db.joke.findUnique({
+    where: { id: params.jokeId },
+  });
+  if (!joke) throw new Error("Joke not found");
+
+  const data: LoaderData = { joke };
+  return data;
+};
+
 export default function JokeRoute() {
+  const data = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
-      <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
-      </p>
+      <p>{data.joke.content}</p>
     </div>
   );
 }
